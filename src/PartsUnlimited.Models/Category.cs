c8b702d62ebebfff07af52f3386d43e57c12e941
teamleadlabs <demoecde@gmail.com>
2024-08-30 02:39:29 -0700

    New changes Fixes #1

--- src/PartsUnlimited.Models/Category.cs
+++ src/PartsUnlimited.Models/Category.cs
@@ -9,7 +9,7 @@ namespace PartsUnlimited.Models
     public class Category
     {
         public int CategoryId { get; set; }
-
+        //github change
         [Required]
         public string Name { get; set; }
 
